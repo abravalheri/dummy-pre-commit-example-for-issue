5f41cb05e80b57f77a4d6310891da07c0c1d6036
Anderson Bravalheri <andersonbravalheri@gmail.com>
2018-06-13 15:04:10 +0000

    After extra dirs - flake8 hook working not as expected...

---
+++ src/pre_commit_flake8_error_demo/vendored/example.py
@@ -0,0 +1,11 @@
+import sys
+# and then
+from __future__ import print_function
+
+def lets_do_lots_of_pep8_violations_in_this_file_just_for_demonstrations_purpose( kwargs = 'with spaces around?' ):
+
+    class SomeClass:
+        def with_not_enough_methods():
+            pass
+
+lets_do_lots_of_pep8_violations_in_this_file_just_for_demonstrations_purpose = 3
